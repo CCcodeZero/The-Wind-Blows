a2882c0ce263e18184262c9a2301acfda100c8a9
imdonghun <psyspcc@naver.com>
2021-07-25 02:54:53 +0900

    초기 커밋.

---
+++ The Wind Blows/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Wind_Blows_
+{
+    public class Cat // pubilc(공공의) <-> 반대 internal(내부의)
+    {
+        public string Name;
+        public int Age;
+        private int Happiness; // 행복지수 (프로그램 내 다른 기능에 의해서 능동적이게 변화 가능성 열어둠. private(사적인).
+
+        public void Play() // function (기능)
+        {
+
+        }
+    }
+}
